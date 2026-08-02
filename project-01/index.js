fe01e1326b1620e9866b1a632b8151905a108c18
satyam294 <satyamchand294@gmail.com>
2026-08-02 19:51:35 +0530

    custom middleware for creating request logs

--- project-01/index.js
+++ project-01/index.js
@@ -11,6 +11,12 @@ const PORT = 8000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  fs.appendFile('./log.txt', `${Date.now()} : ${req.method} : ${req.path}\n`, (err, data) => {
+    next();
+  });
+})
+
 // routes
 
 /*
